6dbacbde5ae7187395addfe1f33d6e7b6dd0f765
lyhhh <17610603221@163.com>
2020-11-06 23:55:44 +0800

    deploy

---
+++ precache-manifest.95bbd710b9ef9acb23649a686e600a28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583b77aefd850f316163",
+    "url": "/moneybag-website/css/app.19d03e77.css"
+  },
+  {
+    "revision": "0df903533b38eb45ea3ca07bbbd1ca2e",
+    "url": "/moneybag-website/icon.png"
+  },
+  {
+    "revision": "5b982123a7a47d1bd12ba4adcd426284",
+    "url": "/moneybag-website/index.html"
+  },
+  {
+    "revision": "583b77aefd850f316163",
+    "url": "/moneybag-website/js/app.a0929481.js"
+  },
+  {
+    "revision": "8cfdb0dc498768a6a24c",
+    "url": "/moneybag-website/js/chunk-vendors.ae148881.js"
+  },
+  {
+    "revision": "eee8f8bfdaeecd69b8249832b6b6aa9a",
+    "url": "/moneybag-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moneybag-website/robots.txt"
+  }
+]);
